c30beac0cba4860e659a45cbe54982d693566224
Austin Hudson <arhudson7@gmail.com>
2016-07-18 17:51:01 -0400

    you can add places

--- back-end/server.js
+++ back-end/server.js
@@ -13,3 +13,43 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+// connect to the database server!
+var url = 'mongodb://localhost:27017/locations'
+mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app. another way to start a server in express
+  var server = app.listen(process.env.PORT || 3000, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// when things go wrong
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+
+app.post("/locations", function(req, res) {
+
+  var newLocation = req.body;
+
+  //insert one new contact
+  db.collection(LOCATION_COLLECTION).insert(newLocation, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new place.");
+    } else {
+      res.status(201).json(doc);
+    }
+  });
+});
--- front-end/main.js
+++ front-end/main.js
@@ -1,35 +1,81 @@
 document.addEventListener("DOMContentLoaded", function(){
 console.log("main.js loaded");
 
-var locationBtn = document.querySelector("#location-btn");
 var searchBtn = document.querySelector("#search-location");
-
 
 var apiPublicKeyQuery = "?key=" + API_KEY;
 var map;
-var longitude = -73.9895971, latitude = 40.7399767; //GA'S LOCATION
-
+var longitude, latitude; //GA'S LOCATION
+var currentLocation;
+var userInput;
+var service;
+var infoWindow;
 
 function initMap() {
   // GA's location
+  currentLocation =  new google.maps.LatLng(latitude,longitude);
   map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(latitude,longitude),
+    center: currentLocation,
     zoom: 15
   });
+  var request = {
+    location: currentLocation,
+    radius: '30',
+    query: userInput
+  }
 
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function success(pos) {
+  latitude = pos.coords.latitude;
+  longitude = pos.coords.longitude;
+  initMap();
+}
+function error(){
 
 }
 
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
+function createMarker(res) {
+    var marker = new google.maps.Marker({
+      position: res.geometry.location,
+      map: map
+    })
+    infoWindow = new google.maps.InfoWindow({
+        content: ""
+    });
+    google.maps.event.addListener(marker, "click", function(){
+      infoWindow.setContent(res.name + "<button type='button' id='fav'>Fav it!</button");
+      infoWindow.open(map, this);
 
+      document.querySelector("#fav").addEventListener("click", function(){
+        var location = {
+          name: res.name,
+          address: res.formatted_address
+        }
+        console.log(location);
+        $.post('http://localhost:3000/locations', location, function(response){
+          console.log("Response:", response);
+        })
+      })
+
+    });
+
+  }
 searchBtn.addEventListener("click", function(){
-  var userInput = document.querySelector("#location-text").value;
-  console.log(userInput);
-
+  navigator.geolocation.getCurrentPosition(success,error);
+  userInput = document.querySelector("#location-text").value;
 });
-locationBtn.addEventListener("click", function(){
-  //get location
-  initMap();
 
-});
+
 
 });
